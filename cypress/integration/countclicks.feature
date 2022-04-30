e6f639d50bd6033ad734652b28e31ba3035b6ef5
Roman <omishe87@gmail.com>
2022-04-30 19:56:08 +0300

    add new logic to check clicks before and after default url

--- cypress/integration/countclicks.feature
+++ cypress/integration/countclicks.feature
@@ -1,5 +1,9 @@
 Feature: Count new clicks
 
+    # @loggedIn
+    # Scenario: Look for current amount of clicks
+    #     Then I see amount of click on analytic page
+
     @loggedIn
     Scenario: New click was added in report after generated click
         Then new click is shown in report
--- cypress/support/step_definitions/countclicks.js
+++ cypress/support/step_definitions/countclicks.js
@@ -1,18 +1,39 @@
-// const howmanyClicksBefore = () => {
-//   cy.
+// const checkClicks = () => {
+//   cy.visit('analytics/statistics')
+//   cy.get('[type=submit]').click()
+//   cy.get('[data-row-index="1"][data-column-index ="2"]').invoke('text')
 // }
 
+// Then('I see amount of click on analytic page', () => {
+//   expect(clicksBefore).not.to.be.empty
+// })
+
 Then('new click is shown in report', () => {
+  cy.visit('analytics/statistics')
+  cy.get('[type=submit]').click()
+  cy.get('[data-row-index="1"][data-column-index ="2"]')
+    .invoke('text')
+    .then(parseInt)
+    .as('before')
+
   cy.visit('overview/dashboard')
-  cy.get('.buttons--lA5HM.after--2rVdf').click()
 
   cy.get('.url--1isxt').invoke('text').as('fromClipboard')
 
   cy.get('@fromClipboard').then((f) => {
     cy.origin('https://ru.superchat.live/', { args: { f } }, ({ f }) => {
       cy.visit(f)
-    })
+    }).wait(4000)
   })
-  cy.visit('analytics/statistics')
+
+  cy.visit('analytics/statistics').wait(6000)
   cy.get('[type=submit]').click()
+
+  cy.get('[data-row-index="1"][data-column-index ="2"]')
+    .invoke('text')
+    .then(parseInt)
+    .as('after')
+    .then(function () {
+      expect(this.after).to.be.eq(this.before + 1)
+    })
 })
